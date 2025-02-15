2c820d17240fa189112092ae71a3322949c986a7
Wesley Lima <wesleyguedes7@gmail.com>
2025-02-15 11:58:16 -0300

    Mega Fama

---
+++ src/screens/WebView/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  loadingContainer: {
+    alignItems: "center",
+    backgroundColor: "#37353A",
+    height: "100%",
+    justifyContent: "center",
+  },
+});
